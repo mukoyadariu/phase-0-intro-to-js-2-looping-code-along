068c5e2ac6c08a3f9196664a32d7327cecf3fd40
mukoyadarius <mukoya.darius@student.moringaschool.com>
2023-06-16 13:25:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names=['Guadalupe','Ollie','Aki'];
+const occasion = "surprise";
+
+ 
+ function writeCards( names, occasion ) {
+  let personalCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    personalCards.push( `Thank you, ${names[i]}, for the wonderful ${occasion} gift!` )
+  }
+  return personalCards
+}
+
+function countDown(){
+    let numCountdown=11;
+    while(numCountdown--){
+      console.log(numCountdown)
+    }
+  } 
